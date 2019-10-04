c6cea57da9e11d6857195b50511205037aa288d3
Daniel C <daniel@meteorspark.com>
2019-10-04 19:02:40 +0700

    Improve getPackageFullName

--- functions/meteor.bash
+++ functions/meteor.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 getPackageFullName () {
-  local package_path="$1"
+    local package_path="$1"
 
-  cat "$package_path" | grep -m 1 name | sed -e $'s/\s\+name:\s*["\']\(.\+\)["\'].*/\\1/g'
+    cat "$package_path" | grep -m 1 name | platformSed -e $'s/\s\+"\\?name"\\?:\s*["\']\(.\+\)["\'].*/\\1/g'
 }
