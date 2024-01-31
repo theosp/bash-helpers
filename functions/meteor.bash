3f5fc075f21db5751d3284b9c2ad217ac7148c30
Daniel C <daniel@meteorspark.com>
2024-01-31 14:06:58 +0700

    Add meteor-x86_64

--- functions/meteor.bash
+++ functions/meteor.bash
@@ -5,3 +5,7 @@ getPackageFullName () {
 
     cat "$package_path" | grep -m 1 name | platformSed -e $'s/\s\+"\\?name"\\?:\s*["\']\(.\+\)["\'].*/\\1/g'
 }
+
+meteor-x86_64 () {
+    NODE_TLS_REJECT_UNAUTHORIZED="0" arch -x86_64 meteor "$@"
+}
